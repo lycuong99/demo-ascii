ba731f1ad155fe6193e45b1c84aa714929cca584
lycuong99 <lycuong99@gmail.com>
2024-07-17 15:50:19 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { AsciiRenderer, OrbitControls, PerspectiveCamera, useHelper } from "@react-three/drei";
+import "./App.css";
+import { PointLightHelper, SpotLightHelper } from "three";
+
+function App() {
+  return (
+    <main>
+      <Canvas className="canvas">
+        <Scene />
+      </Canvas>
+    </main>
+  );
+}
+
+export default App;
+
+function Torusknot(props) {
+  const ref = useRef();
+  const viewport = useThree((state) => state.viewport);
+  useFrame((state, delta) => (ref.current.rotation.x = ref.current.rotation.y += delta / 2));
+  return (
+    <mesh scale={Math.min(viewport.width, viewport.height) / 5} {...props} ref={ref}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshNormalMaterial color="#ffff12" flatShading />
+    </mesh>
+  );
+}
+
+function Scene() {
+  const spotLightRef = useRef();
+  const pointLightRef = useRef();
+  // useHelper(pointLightRef, PointLightHelper);
+  // useHelper(spotLightRef, SpotLightHelper);
+  return (
+    <>
+      <OrbitControls />
+      <color attach="background" args={["#000"]} />
+      <spotLight position={[2, 3, 5]} angle={0.55} intensity={30} penumbra={1} ref={spotLightRef} />
+
+      {/* <pointLight intensity={12} position={[-5, 5, -5]} ref={pointLightRef} /> */}
+      {/* <pointLight intensity={12} position={[10, 10, 10]} /> */}
+      <ambientLight intensity={1} />
+      <Torusknot />
+      <AsciiRenderer fgColor="black" bgColor="transparent" characters=".:+=#%@" />
+    </>
+  );
+}
