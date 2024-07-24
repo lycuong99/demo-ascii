c8d7d3f7d1bff770b94658ef58a59090af39969f
lycuong99 <lycuong99@gmail.com>
2024-07-24 16:42:40 +0700

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily:{
+      neu: ['"Neue Machina"', 'Helvetica', 'Arial','sans-serif'],
+    },
+    extend: {
+      
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
