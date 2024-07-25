12222ecd8eca72b1cb342441285f582552a092ba
lycuong99 <lycuong99@gmail.com>
2024-07-25 22:46:29 +0700

    update intro animation text with gradient color

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/prop-types */
+import gsap from "gsap";
 import { memo, useEffect, useLayoutEffect, useRef } from "react";
 import Splitting from "splitting";
+import { cn } from "./lib/util";
+import Button from "./components/button";
 
 class Line {}
 
@@ -84,167 +88,183 @@ const lettersAndSymbols = [
 function getRandomChar() {
   return lettersAndSymbols[Math.floor(Math.random() * lettersAndSymbols.length)];
 }
+
+function createTextAnimation(intro) {
+  const results = Splitting({
+    target: intro,
+    by: "lines",
+  });
+
+  console.log(results);
+  results.forEach((s) => Splitting({ target: s.words }));
+
+  const lines = results[0].lines.map((line, index) => {
+    const words = line;
+    const cells = words
+      .flatMap((word, index) => {
+        const chars = word.querySelectorAll(".char");
+
+        return [...chars];
+      })
+      .map((char, index) => {
+        return new Cell({
+          el: char,
+          position: index,
+          value: char.textContent,
+          char: char.textContent,
+        });
+      });
+
+
+    return {
+      position: index,
+      el: line,
+      cells: cells,
+    };
+  });
+
+  const clear = () => {
+    lines.forEach((line) => {
+      line.cells.forEach((cell) => {
+        cell.set("&nbsp;");
+      });
+    });
+  };
+  clear();
+
+  const animate = () => {
+    // intro.classList.remove("opacity-0");
+    const MAX_CELL_ITERATION = 45;
+
+    const loop = (line, cell, iteration = 0) => {
+      const prev = cell.position === 0 ? 0 : cell.position - 1;
+
+      cell.cache = cell.char;
+      if (iteration === MAX_CELL_ITERATION - 1) {
+        cell.reset();
+      } else if (cell.position === 0) {
+        const randomChar =
+          iteration < 9 ? ["*", "-", "\u0027", "\u0022"][Math.floor(Math.random() * 4)] : getRandomChar();
+        cell.set(randomChar);
+      } else {
+        // el.textContent = cache;
+        cell.set(line.cells[prev].cache);
+      }
+
+      if (cell.cache != "&nbsp;") {
+        ++iteration;
+      }
+
+      if (iteration < MAX_CELL_ITERATION) {
+        setTimeout(() => {
+          loop(line, cell, iteration);
+        }, 16);
+      }
+    };
+    lines.forEach((line, i) => {
+      line.cells.forEach((cell) => {
+        setTimeout(() => {
+          loop(line, cell);
+        }, (i + 1) * 200);
+      });
+    });
+  };
+  const reset = () => {
+    lines.forEach((line) => {
+      line.cells.forEach((cell) => {
+        cell.reset();
+      });
+    });
+  };
+  return {
+    clear,
+    animate,
+    reset,
+  };
+}
 const Intro = () => {
   console.log("hello");
   const introRef = useRef(null);
+  const h1Ref = useRef(null);
   useLayoutEffect(() => {
     console.log("bye");
     const intro = introRef.current;
+    const h1 = h1Ref.current;
     // intro.classList.add("opacity-0");
 
-    const results = Splitting({
-      target: intro,
-      by: "lines",
-    });
-    results.forEach((s) => Splitting({ target: s.words }));
-
-    const lines = results[0].lines.map((line, index) => {
-      const words = line;
-      const cells = words
-        .flatMap((word, index) => {
-          const chars = word.querySelectorAll(".char");
-
-          return [...chars];
-        })
-        .map((char, index) => {
-          return new Cell({
-            el: char,
-            position: index,
-            value: char.textContent,
-            char: char.textContent,
-          });
-        });
-
-      console.table(cells);
-
-      return {
-        position: index,
-        el: line,
-        cells: cells,
-      };
-    });
-
-    const clear = () => {
-      lines.forEach((line) => {
-        line.cells.forEach((cell) => {
-          cell.set("&nbsp;");
-        });
-      });
-    };
-    clear();
-
-    const animate1 = () => {
-      // intro.classList.remove("opacity-0");
-      const MAX_CELL_ITERATION = 45;
-
-      const loop = (line, cell, iteration = 0) => {
-        const prev = cell.position === 0 ? 0 : cell.position - 1;
-
-        cell.cache = cell.char;
-        if (iteration === MAX_CELL_ITERATION - 1) {
-          cell.reset();
-        } else if (cell.position === 0) {
-          const randomChar =
-            iteration < 9 ? ["*", "-", "\u0027", "\u0022"][Math.floor(Math.random() * 4)] : getRandomChar();
-          cell.set(randomChar);
-        } else {
-          // el.textContent = cache;
-          prev === 32 && console.log(prev, cell.cache);
-          cell.set(line.cells[prev].cache);
-        }
-
-        if (cell.cache != "&nbsp;") {
-          ++iteration;
-        }
-
-        if (iteration < MAX_CELL_ITERATION) {
-          setTimeout(() => {
-            loop(line, cell, iteration);
-          }, 16);
-        }
-      };
-      lines.forEach((line, i) => {
-        line.cells.forEach((cell) => {
-          setTimeout(() => {
-            loop(line, cell);
-          }, (i + 1) * 200);
-        });
-      });
-    };
-
-    const animate2 = () => {
-      const MAX_CELL_ITERATION = 45;
-
-      const loop = (line, cell, iteration = 0) => {
-        const el = cell.el;
-        const prev = cell.position === 0 ? 0 : cell.position - 1;
-
-        cell.cache = cell.char;
-        if (iteration === MAX_CELL_ITERATION - 1) {
-          cell.reset();
-        } else if (cell.position === 0) {
-          cell.set(["*", ":"][Math.floor(Math.random() * 2)]);
-        } else {
-          // el.textContent = cache;
-          cell.set(line.cells[prev].cache);
-        }
-
-        if (cell.cache != "&nbsp;") {
-          ++iteration;
-        }
-
-        if (iteration < MAX_CELL_ITERATION) {
-          setTimeout(() => {
-            requestAnimationFrame(() => loop(line, cell, iteration));
-          }, 16);
-        }
-      };
-      lines.forEach((line, i) => {
-        line.cells.forEach((cell) => {
-          setTimeout(() => {
-            requestAnimationFrame(() => loop(line, cell));
-          }, Math.abs(lines.length / 2 - i) * 400);
-        });
-      });
-    };
+    const { animate, clear, reset } = createTextAnimation(intro);
+    const { animate: animate1, reset: reset1 } = createTextAnimation(h1);
 
     let timeout = setTimeout(() => {
+      animate();
+    }, 900);
+    let timeout1 = setTimeout(() => {
       animate1();
-    }, 1000);
+    }, 300);
+    {
+      gsap.set(".flair", { xPercent: -50, yPercent: -50 });
 
-    console.log(lines);
+      let xTo = gsap.quickTo(".flair", "x", { duration: 0.6, ease: "power3" }),
+        yTo = gsap.quickTo(".flair", "y", { duration: 0.6, ease: "power3" });
+
+      window.addEventListener("mousemove", (e) => {
+        xTo(e.clientX - document.querySelector(".flair").offsetWidth / 2);
+        yTo(e.clientY);
+      });
+    }
+    {
+      const spanGradients = document.querySelectorAll(".span-gradient > .words");
+      spanGradients.forEach((spanGradient) => {
+        const gradientChars = spanGradient.querySelectorAll(".word > *");
+
+        let offset = 0;
+
+        gradientChars.forEach(function (char, i) {
+          //bua
+          const charParentWidth = char.parentElement.offsetWidth * 3;
+          char.style.backgroundSize = charParentWidth + "px 100%";
+
+          offset += char.previousElementSibling?.offsetWidth || 0;
+
+          char.style.backgroundPosition = charParentWidth - offset + "px 0%";
+
+          // console.log(char.parentElement.offsetWidth, offset);
+        });
+      });
+    }
 
     return () => {
       clearTimeout(timeout);
+      clearTimeout(timeout1);
       // intro.classList.add("opacity-0");
-      lines.forEach((line) => {
-        line.cells.forEach((cell) => {
-          cell.reset();
-        });
-      });
+      reset();
+      reset1();
     };
   }, []);
 
   return (
     <section
-      className="flex flex-col justify-end h-full"
+      className="container flex flex-col justify-end h-full pb-20"
       style={{
         zIndex: 10,
         position: "relative",
       }}
     >
-      <div className="container flex flex-col items-start justify-center">
-        <h1 className="font-neu font-normal text-[70px] ">
-          <span>The Biggest</span> Decentralized Data<span></span> <span>Warehouse.</span>
+      <div className="container font-neu flex flex-col items-start justify-center">
+        <h1 ref={h1Ref} className="font-neu font-normal text-[70px] uppercase mb-6 h1-text">
+          <div className="text-linear-1 span-gradient">The Biggest</div>{" "}
+          <span className="text-linear-2 span-gradient">Decentralized</span> <span className="font-bold">Data</span>
+          <div></div> <div className="font-bold text-linear-1 span-gradient">Warehouse.</div>
         </h1>
-        <p ref={introRef} className="anim">
-          Lorem ipsum dolor sit amet consectetur.{" "}
+        <p ref={introRef} className="anim text-[20px] text-[#FECE00] uppercase">
+          Lorem ipsum dolor sit amet consectetur.
         </p>
       </div>
-      <button className="bg-transparent">GET ACCESS</button>
+      <Button className="bg-transparent flair absolute top-0 left-0 font-bold">GET ACCESS</Button>
     </section>
   );
 };
+
 const IntroMemo = memo(Intro);
 IntroMemo.displayName = "Intro";
 export default IntroMemo;
---
+++ src/components/button.jsx
@@ -0,0 +1,76 @@
+import { cn } from "../lib/util";
+
+const TopRightCorner = () => {
+  return (
+    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 0H6.66122H7.45031H7.66122L7.66121 7.66127L6.66121 7.66127L6.66122 1H0V0Z"
+        fill="#FECE00"
+      />
+    </svg>
+  );
+};
+
+const TopLeftCorner = () => {
+  return (
+    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.49994 0H0V0.5V1V6.5H1L1 1H6.49994H6.99994V0H6.49994Z"
+        fill="#FECE00"
+      />
+    </svg>
+  );
+};
+
+const BottomRightCorner = () => {
+  return (
+    <svg width="7" height="8" viewBox="0 0 7 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+          fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5.74212 0.661255L5.74213 6.16125H0.242188V7.16125L5.74213 7.16125H6.48434H6.74213L6.74212 0.661255H5.74212Z"
+        fill="#FECE00"
+      />
+    </svg>
+  );
+};
+
+const BottomLeftCorner = () => {
+  return (
+    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.00001 0.5L1 5.99998L6.49994 5.99998V6.99998L1 6.99998H0.257789H0L5.25381e-06 0.5H1.00001Z"
+        fill="#FECE00"
+      />
+    </svg>
+  );
+};
+const Button = ({ children, className, ...props }) => {
+  return (
+    <button className={cn("bg-transparent block outline-none", className)} {...props}>
+      <div className="relative w-full h-full py-3 px-8">
+        <span className="absolute top-0 right-0">
+          <TopRightCorner />
+        </span>
+        <span className="absolute top-0 left-0">
+          <TopLeftCorner />
+        </span>
+        <span className="absolute bottom-0 right-0">
+          <BottomRightCorner />
+        </span>
+        <span className="absolute bottom-0 left-0">
+          <BottomLeftCorner />
+        </span>
+        {children}
+      </div>
+    </button>
+  );
+};
+
+export default Button;
